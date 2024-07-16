8e4484ae38ee660b7e93a331dddba186185c88d9
unknown <38992201+richgrov@users.noreply.github.com>
2024-07-15 22:24:53 -0600

    Pretty-print evaluation table

--- Evaluator.cs
+++ Evaluator.cs
@@ -2,42 +2,112 @@ namespace Woof;
 
 internal class Evaluator : Visitor<bool>
 {
-    private Dictionary<string, bool> _vars;
+    private string[] _vars;
+    private Dictionary<string, bool> _values = new();
+    private bool _printingHeader = false;
 
-    public Evaluator(Dictionary<string, bool> vars)
+    private HashSet<string> _seenColunms = new();
+
+    public Evaluator(params string[] vars)
     {
         _vars = vars;
+        foreach (string var in _vars)
+        {
+            _values[var] = false;
+        }
     }
 
-    public bool Eval(IExpr expr) => expr.Visit(this);
-
-    public bool GetVariable(string id)
+    public void Eval(IExpr expr)
     {
-        return _vars[id];
+        PrintRow(expr, header: true);
+        EvalCombinations(0, expr);
+    }
+
+    private void EvalCombinations(int varIndex, IExpr expr)
+    {
+        if (varIndex >= _vars.Length)
+        {
+            PrintRow(expr, header: false);
+            return;
+        }
+
+        string variable = _vars[varIndex];
+        _values[variable] = true;
+        EvalCombinations(varIndex + 1, expr);
+        _values[variable] = false;
+        EvalCombinations(varIndex + 1, expr);
+    }
+
+    private void PrintRow(IExpr expr, bool header)
+    {
+        _printingHeader = header;
+        _seenColunms.Clear();
+        expr.Visit(this);
+        Console.WriteLine();
+    }
+
+    private void PrintCell(IExpr expr, bool value)
+    {
+        string exprString = expr.ToString()!;
+        if (!_seenColunms.Add(exprString))
+        {
+            return;
+        }
+
+        if (_seenColunms.Count > 1)
+        {
+            Console.Write(" | ");
+        }
+
+        if (_printingHeader)
+        {
+            Console.Write(exprString);
+        }
+        else
+        {
+            Console.Write(value ? "T" : "F");
+            Console.Write(new string(' ', exprString.Length - 1));
+        }
     }
 
     public bool VisitVariable(VariableExpr expr)
     {
-        return _vars[expr.c.ToString()];
+        bool result = _values[expr.c.ToString()];
+        PrintCell(expr, result);
+        return result;
     }
 
     public bool VisitNot(NotExpr expr)
     {
-        return !expr.expr.Visit(this);
+        bool result = !expr.expr.Visit(this);
+        PrintCell(expr, result);
+        return result;
     }
 
     public bool VisitImplication(ImplicationExpr expr)
     {
-        return !expr.left.Visit(this) || expr.right.Visit(this);
+        bool result = !expr.left.Visit(this);
+        result = expr.right.Visit(this) || result;
+
+        PrintCell(expr, result);
+        return result;
     }
 
     public bool VisitOr(OrExpr expr)
     {
-        return expr.left.Visit(this) || expr.right.Visit(this);
+        bool result = expr.left.Visit(this);
+        result = expr.right.Visit(this) || result;
+
+        PrintCell(expr, result);
+        return result;
     }
 
     public bool VisitAnd(AndExpr expr)
     {
-        return expr.left.Visit(this) && expr.right.Visit(this);
+        bool result = expr.left.Visit(this);
+        result = expr.right.Visit(this) && result;
+
+        PrintCell(expr, result);
+        return result;
     }
 }
--- Program.cs
+++ Program.cs
@@ -26,29 +26,7 @@ internal class Program
             IExpr expr = Parser.ParseExpr(tokens);
             Console.WriteLine(expr);
 
-            string[] vars = { "p", "q" };
-            EvaluateAll(vars, 0, new bool[] { false, false }, expr);
+            new Evaluator("p", "q").Eval(expr);
         }
-    }
-
-    public static void EvaluateAll(string[] vars, int index, bool[] values, IExpr expr)
-    {
-        if (index >= vars.Length)
-        {
-            var varDict = new Dictionary<string, bool>();
-            for (int i = 0; i < vars.Length; i++)
-            {
-                varDict[vars[i]] = values[i];
-            }
-
-            var eval = new Evaluator(varDict);
-            Console.WriteLine(eval.Eval(expr));
-            return;
-        }
-
-        values[index] = true;
-        EvaluateAll(vars, index + 1, values, expr);
-        values[index] = false;
-        EvaluateAll(vars, index + 1, values, expr);
     }
 }
