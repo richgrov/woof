82bc692fe5d30f55dce8a1aeb3128d550188e476
unknown <38992201+richgrov@users.noreply.github.com>
2024-07-28 15:46:58 -0600

    Add tests for parsing basic expression units

---
+++ Woof.Tests/ParseTest.cs
@@ -0,0 +1,58 @@
+namespace Woof.Tests;
+
+[TestClass]
+public class ParseTest
+{
+    [TestMethod]
+    public void TestVariable()
+    {
+        Assert.AreEqual(new VariableExpr("p"), ToExpr("p"));
+    }
+
+    [TestMethod]
+    public void TestNot()
+    {
+        Assert.AreEqual(new NotExpr(new VariableExpr("p")), ToExpr("~p"));
+    }
+
+    [TestMethod]
+    public void TestImplication()
+    {
+        Assert.AreEqual(new ImplicationExpr(new VariableExpr("p"), new VariableExpr("q")), ToExpr("p->q"));
+    }
+
+    [TestMethod]
+    public void TestDoubleImplication()
+    {
+        Assert.AreEqual(new DoubleImplicationExpr(new VariableExpr("p"), new VariableExpr("q")), ToExpr("p<->q"));
+    }
+
+    [TestMethod]
+    public void TestOr()
+    {
+        Assert.AreEqual(new OrExpr(new VariableExpr("p"), new VariableExpr("q")), ToExpr("pvq"));
+    }
+
+    [TestMethod]
+    public void TestAnd()
+    {
+        Assert.AreEqual(new AndExpr(new VariableExpr("p"), new VariableExpr("q")), ToExpr("p^q"));
+    }
+
+    private static IExpr ToExpr(string input)
+    {
+        var tokens = new List<Token>();
+        var tokenizer = new Tokenizer(input);
+        while (true)
+        {
+            var tok = tokenizer.Next();
+            tokens.Add(tok);
+            if (tok.type == TokenType.Eof)
+            {
+                break;
+            }
+        }
+
+        return Parser.ParseExpr(tokens);
+    }
+}
